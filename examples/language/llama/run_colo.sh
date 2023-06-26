ebddf24f119d9ca8e10d407213cbef1d5c95c741
Yanming Wang <yanmwang@amazon.com>
2023-06-26 20:35:57 +0000

    Use bf16 by default, add grad accumulation training script

--- examples/language/llama/run_colo.sh
+++ examples/language/llama/run_colo.sh
@@ -6,11 +6,11 @@ colossalai run --nproc_per_node 8 train_llama.py \
     --num_train_epochs 3 \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 8 \
+    --gradient_accumulation_steps 16 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 2000 \
     --save_total_limit 1 \
-    --learning_rate 2e-6 \
+    --learning_rate 5e-6 \
     --weight_decay 0. \
-    --warmup_ratio 0.03 \
+    --warmup_ratio 0.03
