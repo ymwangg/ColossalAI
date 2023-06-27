5c8480eff7f7c9784f45dbde7d1bf7782668226b
Yanming Wang <yanmwang@amazon.com>
2023-06-27 21:51:55 +0000

    Add gradient checkpointing

--- examples/language/llama/run_colo.sh
+++ examples/language/llama/run_colo.sh
@@ -4,13 +4,13 @@ colossalai run --nproc_per_node 8 train_llama.py \
     --data_path ./alpaca_data.json \
     --output_dir ./trained/saved_model.pt \
     --num_train_epochs 3 \
-    --per_device_train_batch_size 1 \
+    --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 16 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 2000 \
     --save_total_limit 1 \
-    --learning_rate 5e-6 \
+    --learning_rate 1e-5 \
     --weight_decay 0. \
     --warmup_ratio 0.03
